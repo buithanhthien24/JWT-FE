1b6d78a73927a917c178e6b0cc90014d5c73609a
thienbt03@gmail.com <thienbt05@gmail.com>
2024-11-11 16:03:57 +0700

    Hello

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { Outlet } from "react-router-dom";
+import Header from "./components/layout/header";
+import axios from "./util/axios.customize";
+import { useEffect } from "react";
+
+function App() {
+  useEffect(() => {
+    const fetchHelloWorld = async () => {
+      const res = await axios.get(`/v1/api`);
+      console.log(">>> check res: ", res);
+    };
+
+    fetchHelloWorld();
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <Outlet />
+    </>
+  );
+}
+
+export default App;
