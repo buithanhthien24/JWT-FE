ac6b23b7a367d0760b662b6a822bf7b26d88c82b
thienbt03@gmail.com <thienbt05@gmail.com>
2024-11-12 21:17:21 +0700

    step 7

--- src/util/api.js
+++ src/util/api.js
@@ -7,6 +7,18 @@ const createUserApi = (name, email, password) => {
     email,
     password,
   };
+
   return axios.post(URL_API, data);
 };
-export { createUserApi };
+
+const loginApi = (email, password) => {
+  const URL_API = "/v1/api/login";
+  const data = {
+    email,
+    password,
+  };
+
+  return axios.post(URL_API, data);
+};
+
+export { createUserApi, loginApi };
